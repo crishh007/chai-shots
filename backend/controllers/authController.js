9993810f1898e8bea9345d26d24302437e1234d6
crishh007 <108343113+crishh007@users.noreply.github.com>
2026-03-14 15:06:07 +0530

    Implement authentication system with JWT, bcrypt, MongoDB and protected routes

---
+++ backend/controllers/authController.js
@@ -0,0 +1,51 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  await user.save();
+
+  res.json({ message: "User registered successfully" });
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+const authMiddleware = require("./middleware/authMiddleware");
 
 app.get("/", (req, res) => {
   res.send("Chai Shots API Running");
 });
+
+app.get("/api/profile", authMiddleware, (req, res) => {
+  res.json({
+    message: "This is a protected route",
+    userId: req.user.id
+  });
+});
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
